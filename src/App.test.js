f596a00d02fca19c250a91eb5a29c1d630a53cbd
Chris Shepherd <chris@chrisshepherd.me>
2020-05-02 21:00:11 +0100

    Move Enyzme setup to separate file

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import App from './App';
+import { shallow } from "enzyme";
+import React from "react";
+import App from "./App";
 
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-it('renders', () => {
+it("renders", () => {
   const div = shallow(<App />);
-  expect(div).toMatchSnapshot();
 });
